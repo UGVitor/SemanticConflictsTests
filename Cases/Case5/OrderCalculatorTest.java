ccdb8a98a8295b65996cbaa437b61be0dad34bb8
Vitor Dias <110608624+UGVitor@users.noreply.github.com>
2025-10-18 14:35:08 -0300

    Left Alteration Test

---
+++ Cases/Case5/OrderCalculatorTest.java
@@ -0,0 +1,14 @@
+
+public class OrderCalculator {
+    public double applyDiscount(double total, boolean isVip) {
+
+        if (isVip) {
+            total = total * 0.9;
+        }
+        return total + calculateShipping(total);
+    }
+
+    private double calculateShipping(double total) {
+        return 15.0;
+    }
+}
