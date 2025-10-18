beff3f8428cc7f1013709dcf9cffdc1102f71598
Vitor Freitas <developer.vitord@gmail.com>
2025-10-18 14:06:11 -0300

    Right version OrderCalculator

--- Cases/Cases6/OrderCalculator.java
+++ Cases/Cases6/OrderCalculator.java
@@ -1,11 +1,11 @@
 public class OrderCalculator {
     public double applyDiscount(double total, boolean isVip) {
-        if (isVip) {
-            total = total * 0.9;
+        double shipping = calculateShipping(total);
+        if (total > 100) {
+            shipping = 0.0;
         }
-        return total + calculateShipping(total);
+        return total + shipping;
     }
-
     private double calculateShipping(double total) {
         return 15.0; // frete fixo
     }
